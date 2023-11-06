70d6b2885aef22a4182489c0f6ba11e0eb97200a
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-11-06 15:33:50 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Leonardo on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
